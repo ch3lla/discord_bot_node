2d8b3b5a9e3cf4cd012ed22b1873e2e063d99a86
ch3lla <chukwumaemmanuella03@gmail.com>
2023-05-07 16:36:49 +0100

    changed the intents to guilds

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ const axios = require('axios');
 const Discord = require('discord.js');
 const keep_alive = require("./keep_alive");
 
-client = new Discord.Client({intents: 3072})
+client = new Discord.Client({intents: [
+    Discord.GatewayIntentBits.Guilds,
+    Discord.GatewayIntentBits.GuildMessages,
+    Discord.GatewayIntentBits.DirectMessages
+]});
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
+
+client.on('message', msg => {
+    console.log(`Received message: ${msg.content} from ${msg.author.tag}`);
+})
 
 client.on('message', async msg => {
     switch (msg.content){
@@ -22,13 +30,16 @@ client.on('message', async msg => {
         case "!eye":
             msg.channel.send("You are now subscribing to eye reminders.");
             interval = setInterval (function () {
-                msg.channel.send("Please take an eye brek now.")
+                msg.channel.send("Please take an eye break now.")
                 .catch(console.error);
             }, 360000);
             break;
         case "!stop":
             msg.channel.send("I have stopped eye reminders.");
             clearInterval(interval);
+            break;
+        case "Hello":
+            msg.channel.send(`Hello ${msg.author.tag}`);
             break;
     }
     
